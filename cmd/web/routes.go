3d3a820b28cab8f751f7b2c5fa85361def71bfab
LUIGI_CAMPAGNOLA <gigicampagnola@gmail.com>
2022-08-26 06:34:11 -0600

    adding project to new repo

---
+++ cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/luigicampagnola/bookings-app/pkg/config"
+	"github.com/luigicampagnola/bookings-app/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+	return mux
+}
